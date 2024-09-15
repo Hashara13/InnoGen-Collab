9901f886c88217eba472da39e819e5dfc1b81d7b
Hashara13 <hnethmin13@gmail.com>
2024-09-15 11:33:07 +0530

    base templates

--- base/views.py
+++ base/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request,'home.html')
+    return render(request,'base/home.html')
 
 def room(request):
     return render(request,'room.html')
