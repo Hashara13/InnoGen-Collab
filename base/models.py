a7fd470cc9415374c1de7f22cbdacd518cbac961
Hashara13 <hnethmin13@gmail.com>
2024-09-15 20:48:07 +0530

    model migrations

--- base/models.py
+++ base/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Topic(models.Model):
+    name=models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+    
 class Room(models.Model):
-    # host=
+    host=models.ForeignKey(User,on_delete=models.SET_NULL, null=True)
     # owner=
-    # topic=
+    topic=models.ForeignKey(Topic,on_delete=models.SET_NULL, null=True)
     name=models.CharField(max_length=300)
     desc=models.TextField(max_length=800,null=True,blank=True)
     updated=models.DateTimeField(auto_now=True)
@@ -11,3 +18,16 @@ class Room(models.Model):
     
     def __str__(self):
         return self.name
+    
+class Message(models.Model):
+     # once delete 1 user contain messages also deleted
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    # once delete 1 chat room contain messages also deleted
+    room=models.ForeignKey(Room,on_delete=models.CASCADE)
+    body=models.TextField()
+    updated=models.DateTimeField(auto_now=True)
+    created=models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        # view 1st 50 characters
+        return self.body[0:50]
--- base/views.py
+++ base/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 
 
 def home(request):
-    return render(request,'base/home.html')
+    
+    #fetch all queys in Room model to view
+    rooms=Room.objects.all()
+    context={'rooms':rooms}
+    return render(request,'base/home.html',context)
 
 def room(request):
-    room=None
-    for i in rooms:
-        if i['id']==int[pk]:
-            room=i
+   #fetch one queys in Room model to view based on single id
+    rooms=Room.objects.get(id=pk)
     context={'room':room}
     return render(request,'/base/room.html',context)
