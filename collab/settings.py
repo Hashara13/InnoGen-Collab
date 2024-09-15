0eb2bb6a5b553160906ee0511dfd6b61cc99a977
Hashara13 <hnethmin13@gmail.com>
2024-09-15 11:16:15 +0530

    minor changes

--- collab/settings.py
+++ collab/settings.py
@@ -44,7 +44,9 @@ ROOT_URLCONF = 'collab.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
