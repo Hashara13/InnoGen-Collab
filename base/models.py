17bda113bf3675a7463bbbd3e91d0110ebaa6cb9
Hashara13 <hnethmin13@gmail.com>
2024-09-15 20:13:24 +0530

    model migration

--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    # host=
+    # owner=
+    # topic=
+    name=models.CharField(max_length=300)
+    desc=models.TextField(max_length=800,null=True,blank=True)
+    updated=models.DateTimeField(auto_now=True)
+    created=models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
--- base/views.py
+++ base/views.py
@@ -7,4 +7,9 @@ def home(request):
     return render(request,'base/home.html')
 
 def room(request):
-    return render(request,'room.html')
+    room=None
+    for i in rooms:
+        if i['id']==int[pk]:
+            room=i
+    context={'room':room}
+    return render(request,'/base/room.html',context)
