600fd22f1773dea434b7f1ec359813a775a4fff7
Hashara13 <hnethmin13@gmail.com>
2024-09-17 23:17:57 +0530

    signup done via django UserCreation

--- base/views.py
+++ base/views.py
@@ -16,7 +16,7 @@ def signinPage(request):
         return redirect('home')
     
     if request.method=='POST':
-        username=request.POST.get('username')
+        username=request.POST.get('username').lower()
         password=request.POST.get('password')
         try:
             user=User.objects.get(username=username)
@@ -39,6 +39,16 @@ def signOutPage(request):
 def signUpPage(request):
     # page='signup'
     form=UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred')
     return render(request,'base/signin_up.html',{'form':form})
 
 def home(request):
