83ee621a7706c2918bbc1c2a317550e5b282ed9d
Hashara13 <hnethmin13@gmail.com>
2024-10-02 21:17:14 +0530

    config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './templates/**/*.html',  
+    './static/**/*.js',      
+    './static/**/*.html',    
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
